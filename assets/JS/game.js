66fc293d11f91c77b2ffa263b1cdc265959a3268
Cristian FC <COFunez97@gmail.com>
2019-10-10 23:47:09 -0400

    added index.html, assets folder with styles, js, images folders

---
+++ assets/JS/game.js
@@ -0,0 +1,30 @@
+let player = {
+    health: 100,
+    Attack: 10,
+    exp: 10,
+}
+
+let en1 = {
+    health: 100,
+    attack: 7,
+    exp: 15,
+}
+
+let en2 = {
+    health: 125,
+    attack: 10,
+    exp: 20,
+}
+
+let en3 = {
+    health: 175,
+    attack: 15,
+    exp: 30,
+}
+
+let boss ={
+    health = 225,
+    attack = 25,
+    exp: 50,
+}
+
