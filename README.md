afb2c56f48f3f3ab56888983a61bf2ba3bdfd12f
Cristian FC <COFunez97@gmail.com>
2019-10-13 00:51:50 -0400

    updated readme, html, style and js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # unit-4-game
+
+Welcome to Rupee Game!
+
+This game assigns each Rupee a different value each game.
+
+Add the Rupees you click on to get your score to match the target score.
+
+If you get the target score you win!
+
+If your score exceeds the value of the target score you lose.
+
+Your wins and losses are logged at the bottom.
+
+Refresh the page to reset your wins and losses.
--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,30 +1,115 @@
-let player = {
-    health: 100,
-    Attack: 10,
-    exp: 10,
+function newTarget() {
+    targetNumber = Math.floor(Math.random() * 120 + 1);
+
+    if (targetNumber > 120 || targetNumber < 32) {
+        newTarget();
+    }
 }
 
-let en1 = {
-    health: 100,
-    attack: 7,
-    exp: 15,
+let userScore = 0;
+let wins = 0;
+let losses = 0;
+
+$("#targetNumber").text("Target number: " + targetNumber);
+
+restart();
+
+function assignValue() {
+    rupeeOptions1 = Math.floor(Math.random() * 12 + 1);
+    rupeeOptions2 = Math.floor(Math.random() * 12 + 1);
+    rupeeOptions4 = Math.floor(Math.random() * 12 + 1);
+    rupeeOptions4 = Math.floor(Math.random() * 12 + 1);
+
+
+
+    let greenRupee = $("<img>");
+
+    greenRupee.addClass("rupee-image");
+
+    greenRupee.attr("src", "assets/images/greenrupee.png");
+
+    greenRupee.attr("data-rupeevalue", rupeeOptions1);
+
+    $("#Rupees").append(greenRupee);
+
+    let blueRupee = $("<img>");
+
+    blueRupee.addClass("rupee-image");
+
+    blueRupee.attr("src", "assets/images/bluerupee.png");
+
+    blueRupee.attr("data-rupeevalue", rupeeOptions2);
+
+    $("#Rupees").append(blueRupee);
+
+    let redrupee = $("<img>");
+
+    redrupee.addClass("rupee-image");
+
+    redrupee.attr("src", "assets/images/redrupee.png");
+
+    redrupee.attr("data-rupeevalue", rupeeOptions4);
+
+    $("#Rupees").append(redrupee);
+
+    let goldRupee = $("<img>");
+
+    goldRupee.addClass("rupee-image");
+
+    goldRupee.attr("src", "assets/images/goldRupee.png");
+
+    goldRupee.attr("data-rupeevalue", rupeeOptions4);
+
+    $("#Rupees").append(goldRupee);
+
+    $(".rupee-image").on("click", function () {
+
+        let rupeeValues = ($(this).attr("data-rupeevalue"));
+
+        rupeeValues = parseInt(rupeeValues);
+
+        userScore += rupeeValues;
+        $("#userScore").text("Your current score is: " + userScore);
+        //
+        if (userScore === targetNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            restart();
+
+
+        } else if (userScore > targetNumber) {
+            losses++;
+            $("#losses").text("Losses: " + losses);
+            restart();
+
+        }
+    });
+};
+
+
+function restart() {
+    $("#Rupees").text("");
+    assignValue();
+    newTarget();
+    $("#targetNumber").text("Target number: " + targetNumber);
+    userScore = 0;
+    $("#userScore").text("Your current score is: " + userScore);
+
+    if (rupeeOptions1 === rupeeOptions2 || rupeeOptions1 === rupeeOptions4 || rupeeOptions1 === rupeeOptions4) {
+        restart();
+    }
+
+    if (rupeeOptions2 === rupeeOptions4 || rupeeOptions2 === rupeeOptions4) {
+        restart();
+    }
+
+    if (rupeeOptions4 === rupeeOptions4) {
+        restart();
+    }
+    console.log(rupeeOptions1);
+    console.log(rupeeOptions2);
+    console.log(rupeeOptions4);
+    console.log(rupeeOptions4);
 }
 
-let en2 = {
-    health: 125,
-    attack: 10,
-    exp: 20,
-}
-
-let en3 = {
-    health: 175,
-    attack: 15,
-    exp: 30,
-}
-
-let boss ={
-    health = 225,
-    attack = 25,
-    exp: 50,
-}
 
